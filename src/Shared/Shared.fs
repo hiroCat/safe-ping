d0b0d2b589e8ad58e1d66d48f48079a940a819dd
Miquel Puigdomenech <Miquel.Puigdomenech@dnvgl.com>
2018-12-27 00:03:49 +0100

    added data structure

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -1,4 +1,30 @@
 namespace Shared
 
-type Counter = { Value : int }
+open System
+
+type Status = OK=0 | KO=1 
+
+type EndpointStatus =
+    {   Name: string
+        Status: Status}
+
+type Enviroment =
+    {   Id: Guid
+        Name: string
+        PingStatus: Status
+        TimeStamp: DateTime
+        EndpointsQ: EndpointStatus list}
+
+type ApiEndpConfig =
+    {   Name: string
+        Endpoint : string
+        Body : string }
+
+type EnviromentConfig =
+    {   Id:Guid
+        Name : string
+        CreationDate : DateTime
+        Ip: string
+        ApiEndpointsConfig: ApiEndpConfig list}
+
 
